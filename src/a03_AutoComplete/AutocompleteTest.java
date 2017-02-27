b4c1a7b62053c44e067bc62918ee955e4995ab96
michaeldey <michaeldey@hotmail.com>
2017-02-26 22:38:13 -0700

    added but not implemented AutocompleteTest.java jUnit test

---
+++ src/a03_AutoComplete/AutocompleteTest.java
@@ -0,0 +1,24 @@
+package a03_AutoComplete;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AutocompleteTest {
+
+	@Test
+	public void testAutocomplete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAllMatches() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNumberOfMatches() {
+		fail("Not yet implemented");
+	}
+
+}
